5090d6fd18489568640d8c7f45fd32b582dacc1c
ELFrancisco4 <offiongfrancis14@gmail.com>
2022-05-26 20:45:48 +0100

    Fixed side scrolling issue

--- src/App.js
+++ src/App.js
@@ -2,19 +2,42 @@ import LoginPage from "./Pages/LoginPage";
 import HomePage from "./Pages/HomePage";
 import { Routes, Route } from "react-router-dom";
 import NewsAPI from "./Services/NewsAPI";
-import Profile from './Pages/Profile';
+import Profile from "./Pages/Profile";
 import NavigationComponent from "./Pages/NavigationComponent";
-import PrivateRoute from './Services/auth/private-route';
+import PrivateRoute from "./Services/auth/private-route";
+import "./App.css";
+
 const App = () => {
   return (
     <div className="App">
-      <NavigationComponent/>
-       <Routes>
-        <Route path="/"  element={<LoginPage />} />
-        <Route path="/home" element={<PrivateRoute><HomePage/></PrivateRoute>} />
-        <Route path="/newsfeed" element={<PrivateRoute><NewsAPI/></PrivateRoute>} />
-        <Route path="/profile" element={<PrivateRoute><Profile/></PrivateRoute>}/>
-      </Routes> 
+      <NavigationComponent />
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route
+          path="/home"
+          element={
+            <PrivateRoute>
+              <HomePage />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/newsfeed"
+          element={
+            <PrivateRoute>
+              <NewsAPI />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/profile"
+          element={
+            <PrivateRoute>
+              <Profile />
+            </PrivateRoute>
+          }
+        />
+      </Routes>
     </div>
   );
 };
